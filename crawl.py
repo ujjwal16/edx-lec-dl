72c81ef17eb70dc8cfc4fd1936dce2f18836c143
ujjwal16 <ujjwal.kalra@live.in>
2014-12-18 17:50:29 +0530

    Web Crwaler with log facility PDF links not supported

--- crawl.py
+++ crawl.py
@@ -1,7 +1,21 @@
 from sys import argv
-import urllib2
 import source_link
+from merge import merge
 script ,link =argv
-source_link.source(link)
+#source_link.source(link)
+first_visit=[link]
+visited=[]
+while first_visit:
+	active=first_visit.pop()
+	if active not in visited:
+		link_list=source_link.source(active)
+		visited.append(active)
+		first_visit=merge(first_visit,link_list)
+		# print "===================================="
+		# print "merged list "+"\n".join(first_visit)
+		# print "===================================="
+print visited
+
+
 
 
--- source_link.py
+++ source_link.py
@@ -1,17 +1,31 @@
 import urllib2
 import extract
 def source(link):
+	link=link.strip('\'"')
+	log=open("log.txt",'a')	
+	log.write("\n"+link)
+	log.close()
+	check=link.find("ht")
+	if check==-1:
+		link="http://"+link
 	req=urllib2.Request(link)
 	resp=urllib2.urlopen(req)
 	content=resp.read()
+	log1=open("log1.txt",'w')
+	log1.write(content)
 	list_url=[]
 	position=0
-
+	print "this is the active link " + link
 	while True:
 	 	link,position=extract.extract(content,position)
+	 	
+	 	
 	 	if link!=None:
-	 		list_url.append(link)
+	 		pdf=link.find("pdf")
+	 		if pdf == -1:
+	 			list_url.append(link)
 	 	else:
 	 		break
 	 	
-	print "\n".join(list_url)
+	#print "\n".join(list_url)
+	return list_url
